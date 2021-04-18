3cadc1353b42831189d20052f195c13aeef1e5dd
roowahoo <tohyinru95@gmail.com>
2021-04-18 15:10:22 +0000

    added card

--- skincare/src/pages/shop.js
+++ skincare/src/pages/shop.js
@@ -1,29 +1,43 @@
-import React,{useState,useEffect} from 'react'
-import {useLocation} from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import axios from 'axios'
 
-export default function Shop(){
-    const location=useLocation()
+export default function Shop() {
+    const location = useLocation()
     // const username=location.state.form.username
     // const password=location.state.form.password
-    const [products,setProduct]=useState({})
+    const [products, setProduct] = useState([])
 
-    useEffect(()=>{
-        const fetch = async ()=>{
-            const response=await axios.get('https://3000-indigo-orangutan-nf30a8jb.ws-us03.gitpod.io/api/products')
+    useEffect(() => {
+        const fetch = async () => {
+            const response = await axios.get('https://3000-indigo-orangutan-nf30a8jb.ws-us03.gitpod.io/api/products')
             setProduct(response.data)
         }
         fetch()
-    },[])
+    }, [])
 
-    return(
+    return (
         <React.Fragment>
-            <h1>Shop</h1>
-            {products.map(p=>(
-                <h5>{p.name}</h5>
+            <div>
+                <h1>Shop</h1>
 
-            ))}
-            
+                {products.map(p => (
+                    <div className="card">
+                        <img className="card-img-top" src={p.image_url} alt="product_image"></img>
+                        <div className="card-body">
+                            <h4>{p.brand}</h4>
+                            <h5>{p.name}</h5>
+                            <p>{p.description}</p>
+                            <p>$ {p.price}</p>
+                        </div>
+                    </div>
+
+                ))}
+
+
+            </div>
+
+
         </React.Fragment>
     )
 }
