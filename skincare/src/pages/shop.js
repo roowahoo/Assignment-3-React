281b6f5cf65a07b5aa783d7fb5f542443d11d3d0
roowahoo <tohyinru95@gmail.com>
2021-04-22 16:14:33 +0000

    added cat and brnad search filter

--- skincare/src/pages/shop.js
+++ skincare/src/pages/shop.js
@@ -8,7 +8,9 @@ export default function Shop() {
     // const password=location.state.form.password
     const [products, setProduct] = useState([])
     const [isLoggedIn, setIsLoggedIn] = useState({})
-    const [searchSkintype,setSearchSkintype]=useState('')
+    const [searchSkintype, setSearchSkintype] = useState(0)
+    const [searchCategory,setSearchCategory]=useState(0)
+    const [searchBrand,setSearchBrand]=useState(0)
 
     useEffect(() => {
         const fetch = async () => {
@@ -58,21 +60,35 @@ export default function Shop() {
         if (isLoggedIn.id) {
             const response = await axios.get('https://3000-indigo-orangutan-nf30a8jb.ws-us03.gitpod.io/api/bag/' + isLoggedIn.id + '/' + e.target.value + '/add')
             console.log(response.data)
-        }else{
+        } else {
             alert('login')
         }
 
     }
 
-    const search=async(e)=>{
-        if(searchSkintype){
-            const response=await axios.post('https://3000-indigo-orangutan-nf30a8jb.ws-us03.gitpod.io/api/products/skintype',{
-                'skintype_id':parseInt(searchSkintype)
+    const search = async (e) => {
+        if (searchSkintype) {
+            const response = await axios.post('https://3000-indigo-orangutan-nf30a8jb.ws-us03.gitpod.io/api/products/skintype', {
+                'skintype_id': searchSkintype
             })
             setProduct(response.data)
             console.log(response.data)
         }
-       
+        if(searchCategory){
+            const response=await axios.post('https://3000-indigo-orangutan-nf30a8jb.ws-us03.gitpod.io/api/products/category',{
+                'category_id':searchCategory
+            })
+            setProduct(response.data)
+            console.log(response.data)
+        }
+        if(searchBrand){
+            const response=await axios.post('https://3000-indigo-orangutan-nf30a8jb.ws-us03.gitpod.io/api/products/brand',{
+                'brand_id':searchBrand
+            })
+            setProduct(response.data)
+            console.log(response.data)
+        }
+
     }
 
     return (
@@ -80,12 +96,32 @@ export default function Shop() {
             <div>
                 <h1>Shop</h1>
                 <label className='form-label'>Skintype:</label>
-                <select onChange={(e)=>setSearchSkintype(e.target.value)} className='form-control'>
-                    <option value=''>--Select--</option>
+                <select onChange={(e) => setSearchSkintype(e.target.value)} className='form-control'>
+                    <option value=''></option>
                     <option value='1'>Oily</option>
                     <option value='2'>Dry</option>
                     <option value='3'>Combination</option>
                     <option value='4'>Sensitive</option>
+                </select>
+                <label className='form-label'>Category:</label>
+                <select onChange={(e)=>setSearchCategory(e.target.value)} className='form-control'>
+                    <option value=''></option>
+                    <option value='1'>Make-up Remover</option>
+                    <option value='3'>Face Wash</option>
+                    <option value='4'>Toner</option>
+                    <option value='5'>Essence</option>
+                    <option value='6'>Serum</option>
+                    <option value='7'>Lotion</option>
+                    <option value='8'>Cream</option>
+                    <option value='9'>Sunblock</option>
+                </select>
+                <label className='form-label'>Brand:</label>
+                <select onChange={(e)=>setSearchBrand(e.target.value)} className='form-control'>
+                    <option value=''></option>
+                    <option value='1'>Klairs</option>
+                    <option value='3'>Cosrx</option>
+                    <option value='4'>SKIN&LAB</option>
+                    <option value='5'>Rovectin</option>
                 </select>
                 <button onClick={search}>Search</button>
                 <div className='card-div'>
