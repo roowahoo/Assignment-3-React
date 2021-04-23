ec6602a51511f9cf92d003104880ba1fc74efe3c
roowahoo <tohyinru95@gmail.com>
2021-04-23 10:01:29 +0000

    added refresh token

--- skincare/src/App.js
+++ skincare/src/App.js
@@ -1,4 +1,4 @@
-
+import config from './config'
 import './App.css';
 import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -6,7 +6,8 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
-    Link
+    Link,
+    useHistory
 } from 'react-router-dom'
 
 import {
@@ -25,18 +26,37 @@ import Account from './pages/account'
 import Register from './pages/register'
 import Bag from './pages/bag'
 import Review from './pages/review'
-import Logo from './logo.png'
+import Logo from './images/logo.png'
+import ShoppingBag from './images/bag.png'
+import axios from 'axios';
+
 
 function App() {
 
     const [isOpen, setIsOpen] = useState(false);
-
     const toggle = () => setIsOpen(!isOpen);
+    const history = useHistory()
+
+    const logout=async ()=>{
+        localStorage.setItem('accessToken', null);
+        localStorage.setItem('refreshToken', null);
+        history.push('/shop')
+    }
+
+    useEffect(()=>{
+        setInterval(async()=>{
+            const response=await axios.post(config.baseUrl+'api/shoppers/refresh',{
+                refreshToken:localStorage.getItem('refreshToken')
+            })
+            localStorage.setItem('accessToken',response.data.accessToken)
+        },config.refresh_token_interval)
+    })
 
     return (
 
         <Router>
             <div>
+                <Switch>
                 <Navbar color="light" light expand="md" id='navbar'>
                     <NavbarBrand href="/"><img src={Logo} id='logo'/></NavbarBrand>
                     <NavbarToggler onClick={toggle} />
@@ -51,15 +71,17 @@ function App() {
                             <NavItem>
                                 <Link to='/register' className='nav-link'>Register</Link>
                             </NavItem>
-                            <NavItem id='accountIcon m-auto'> 
+                            <NavItem id='accountIcon'> 
                                 <Link to='/account' className='nav-link'>&#128100;</Link>
                             </NavItem>
-                            <NavItem id='bagIcon ml-auto'>
-                                <Link to='/bag' className='nav-link'>&#128092;</Link>
+                            <NavItem id='bagIcon'>
+                                <Link to='/bag' className='nav-link'><img src={ShoppingBag} id='bag'/></Link>
                             </NavItem>
+                            <button onClick={logout}>Logout</button>
                         </Nav>
                     </Collapse>
                 </Navbar>
+            </Switch>
             </div>
             <Switch>
                 <div className='container-fluid p-3'>
