de0fbf485296e6ccc4aee9926fe4d51e8d79f24a
roowahoo <tohyinru95@gmail.com>
2021-04-25 06:01:08 +0000

    added text to home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,1 +1,10 @@
 import React,{useState} from 'react'
+
+export default function Home(){
+    return (
+        <React.Fragment>
+            <h1 className='text'>Welcome to The Skin Shop</h1>
+            <p className='text'>Here, you will find all the skincare products you will ever need!</p>
+        </React.Fragment>
+    )
+}
