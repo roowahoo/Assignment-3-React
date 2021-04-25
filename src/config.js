a797e1593e3cfeae0daa8e3999f98236564f4eef
roowahoo <tohyinru95@gmail.com>
2021-04-25 03:03:31 +0000

    Changed baseurl in config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default{
-    'baseUrl':'https://3000-indigo-orangutan-nf30a8jb.ws-us03.gitpod.io',
+    'baseUrl':'https://theskinshop-vendor.herokuapp.com',
     'refresh_token_interval':5*60*1000
 
 }
